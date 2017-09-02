c95ee41ea8702f8b5ecf12935531b7fabec2e1a5
Edmund Mok <edmundmok@outlook.com>
2017-09-01 21:26:52 -0700

    Initial Commit

---
+++ StringAlgorithms/StringAlgorithms.h
@@ -0,0 +1,19 @@
+//
+//  StringAlgorithms.h
+//  StringAlgorithms
+//
+//  Created by Edmund Mok on 9/1/17.
+//  Copyright © 2017 Edmund Mok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StringAlgorithms.
+FOUNDATION_EXPORT double StringAlgorithmsVersionNumber;
+
+//! Project version string for StringAlgorithms.
+FOUNDATION_EXPORT const unsigned char StringAlgorithmsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringAlgorithms/PublicHeader.h>
+
+
